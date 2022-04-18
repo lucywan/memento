ea536e8a3e2b0fb704831985419d18f1de0ae096
Lucy Wan <lucy.w@berkeley.edu>
2022-04-17 22:09:07 -0700

    EWrewR

--- orig/app/app/settings.py
+++ orig/app/app/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/home/l/lu/luw/public_html/memento/static'
-STATICFILES_DIRS = [
-str(BASE_DIR.joinpath('static'))
-]
+# STATICFILES_DIRS = [
+# str(BASE_DIR.joinpath('static'))
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
